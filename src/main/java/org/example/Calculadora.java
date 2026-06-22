2d29af3c452d6ebaa543e55f1f729078fcedc8d6
Davi <molosse645@gmail.com>
2026-06-22 20:14:08 -0300

    Estrutura inicial - Calculadora.java e Main.java

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora {
+    public int soma(int a, int b) {
+        return a + b;
+    }
+
+    public int divisao(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Divisão por zero!");
+        }
+        return a / b;
+    }
+}
